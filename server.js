b9a75a45847cddf98c4fc064d099d6262a0fbe24
pkrion <pkrion@gmail.com>
2020-10-06 17:12:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const scraper = require('./scraper')
 const app = express();
+    
 
 //Home page
 app.get('/', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     res.sendFile(__dirname + "/index.html");
 });
 
 //API route
 app.get('/api/search', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     scraper.youtube(req.query.q, req.query.page)
         .then(x => res.json(x))
         .catch(e => res.send(e));
