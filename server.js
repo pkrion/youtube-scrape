284ffc0705074df9766c60551a2b0e86edaba8bd
pkrion <pkrion@gmail.com>
2020-10-06 17:20:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,10 @@ const express = require('express');
 const scraper = require('./scraper')
 const app = express();
     
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+var cors = require('cors')
 
-    next();
-}
-
-//...
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: 'cool beans' }));
-    app.use(express.methodOverride());
-    app.use(allowCrossDomain);
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-});
+var app = express()
+app.use(cors())
 
 
 //Home page
