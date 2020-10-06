eb35d607e368351f3c42ef1db225dd0023371b8a
pkrion <pkrion@gmail.com>
2020-10-06 17:15:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const scraper = require('./scraper')
 const app = express();
     
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 //Home page
 app.get('/', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
+   
     res.sendFile(__dirname + "/index.html");
 });
 
